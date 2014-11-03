53e77247f1314eca98c63fe8fce062426f7b49b7
Aleksandr Grinspun <aleksandr.grinspun@rwth-aachen.de>
2014-11-03 19:54:22 +0100

    Initial Commit

---
+++ module/FSMPILoL/config/module.config.php
@@ -0,0 +1,78 @@
+<?php
+namespace FSMPILoL;
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'index' => 'FSMPIVideo\Controller\IndexController'
+        ),
+    ),
+    
+    
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
+        ),
+    ),
+    
+    'translator' => array(
+        'locale' => 'de_DE',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/fsmpi-lol/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    
+	'navigation' => array(
+		'default' => array(
+			array('label' => 'Home', 'route' => 'home'),
+		),
+	),
+    
+    
+	'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
+);
