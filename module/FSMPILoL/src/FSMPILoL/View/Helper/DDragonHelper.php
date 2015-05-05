9ffcfcfcc4e392421afd6c224582e280a862e2c6
Aleksandr <schurix@gmx.de>
2015-05-05 10:58:35 +0200

    Articus/zf2-bower-module needs php 5.5; New DDragon version

--- module/FSMPILoL/src/FSMPILoL/View/Helper/DDragonHelper.php
+++ module/FSMPILoL/src/FSMPILoL/View/Helper/DDragonHelper.php
@@ -4,7 +4,7 @@ namespace FSMPILoL\View\Helper;
 use Zend\View\Helper\AbstractHelper;
 
 class DDragonHelper extends AbstractHelper {
-	protected $version = '5.7.2';
+	protected $version = '5.8.1';
 	protected $base = 'http://ddragon.leagueoflegends.com/cdn/';
 	protected $profileIconPath = 'img/profileicon/';
 	
